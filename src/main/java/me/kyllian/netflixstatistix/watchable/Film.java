3385fa96854b8df60671846c51fb4e603cd16b10
Afrash <32881768+Afrash@users.noreply.github.com>
2019-11-10 14:49:15 +0100

    Film en series klein begin

--- src/main/java/me/kyllian/netflixstatistix/watchable/Film.java
+++ src/main/java/me/kyllian/netflixstatistix/watchable/Film.java
@@ -1,4 +1,20 @@
 package me.kyllian.netflixstatistix.watchable;
 
 public class Film {
+
+    private String title;
+    private int timeDuration;
+    private String genre;
+    private String language;
+    private int ageIndicator;
+
+    public Film(String title, int timeDuration, String genre, String language, int ageIndicator){
+        this.title = title;
+        this.timeDuration = timeDuration;
+        this.genre = genre;
+        this.language = language;
+        this.ageIndicator = ageIndicator;
+    }
+
+
 }
--- src/main/java/me/kyllian/netflixstatistix/watchable/Series.java
+++ src/main/java/me/kyllian/netflixstatistix/watchable/Series.java
@@ -1,4 +1,30 @@
 package me.kyllian.netflixstatistix.watchable;
 
 public class Series {
+
+    private String title;
+    private int timeDuration;
+    private String genre;
+    private String language;
+    private int ageIndicator;
+    private int serialNumber;
+
+    //serie bestaat uit minimaal 1 aflevering en aflevering hoort bij 1 serie
+
+    public Series(String title, int timeDuration, String genre, String language, int ageIndicator, int serialNumber){
+        this.title = title;
+        this.timeDuration = timeDuration;
+        this.genre = genre;
+        this.language = language;
+        this.ageIndicator = ageIndicator;
+        this.serialNumber = serialNumber;
+    }
+
+
+
+
+
+
+
+
 }
