5f817ef9d24fda7675107ca36f5358ab375b1a79
Kyllian <kyllian007@gmail.com>
2019-11-10 11:54:01 +0100

    updated database connection using properties file

--- src/main/java/me/kyllian/netflixstatistix/database/DatabaseHandler.java
+++ src/main/java/me/kyllian/netflixstatistix/database/DatabaseHandler.java
@@ -4,21 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ResourceBundle;
 
 public class DatabaseHandler {
 
     public DatabaseHandler() {
         try {
-            Class.forName("");
-            Connection con = DriverManager.getConnection("", "", "");
+            ResourceBundle resourceBundle = ResourceBundle.getBundle("dbconfig");
+            Class.forName(resourceBundle.getString("db.driver"));
+            Connection con = DriverManager.getConnection(resourceBundle.getString("db.url"), resourceBundle.getString("db.username"), resourceBundle.getString("db.password"));
             Statement stmt = con.createStatement();
             stmt.executeUpdate("CREATE TABLE TEST (NAME VARCHAR(10) NOT NULL )");
         } catch (Exception exception) {
             exception.printStackTrace();
         }
-
-
     }
-
-
 }
