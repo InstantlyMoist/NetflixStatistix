478f1ce2e872f3e0e53cb0c61addd456a7f98ee8
Perunaz <caelan.van.eijnbergen@gmail.com>
2019-11-15 15:56:20 +0100

    episode bijgewerkt

--- src/main/java/me/kyllian/netflixstatistix/watchable/Episode.java
+++ src/main/java/me/kyllian/netflixstatistix/watchable/Episode.java
@@ -3,12 +3,32 @@ package me.kyllian.netflixstatistix.watchable;
 public class Episode {
 
     private int serialNumber;
+    private String title;
+    private int duration;
+    private String description;
 
-    public Episode(int serialNumber){
+    public Episode(int serialNumber, String title, int duration, String description){
         this.serialNumber = serialNumber;
+        this.title = title;
+        this.duration = duration;
+        this.description = description;
     }
 
+    public int getSerialNumber() {
+        return serialNumber;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
 
 
