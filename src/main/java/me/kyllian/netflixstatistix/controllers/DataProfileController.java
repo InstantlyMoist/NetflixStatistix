d9e629f0180ba8fda3fdfad27e6680f66bfcfbe4
Afrash <32881768+Afrash@users.noreply.github.com>
2020-01-12 21:05:37 +0100

    DataProfileController aangemaakt om data te weergeven

---
+++ src/main/java/me/kyllian/netflixstatistix/controllers/DataProfileController.java
@@ -0,0 +1,39 @@
+package me.kyllian.netflixstatistix.controllers;
+import com.sun.org.apache.xpath.internal.patterns.NodeTestFilter;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import me.kyllian.netflixstatistix.exceptions.InputInvalidException;
+import me.kyllian.netflixstatistix.user.User;
+import me.kyllian.netflixstatistix.user.UserBuilder;
+
+import javax.xml.soap.Text;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+
+public class DataProfileController implements Initializable{
+
+    @FXML
+    private ImageView logo;
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        logo.setImage(new Image(getClass().getClassLoader().getResource("assets/logo.png").toExternalForm()));
+
+    }
+
+}
