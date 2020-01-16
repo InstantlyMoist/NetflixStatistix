d5aaf26dc8353ecf4840621a7558da2dfec7def5
InstantlyMoist <kyllian007@gmail.com>
2020-01-16 14:13:24 +0100

    profile loading now works, names get shown

--- src/main/java/me/kyllian/netflixstatistix/controllers/ProfileController.java
+++ src/main/java/me/kyllian/netflixstatistix/controllers/ProfileController.java
@@ -1,5 +1,7 @@
 package me.kyllian.netflixstatistix.controllers;
+
 import com.sun.org.apache.xpath.internal.patterns.NodeTestFilter;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,23 +13,33 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import me.kyllian.netflixstatistix.NetflixStatistix;
 import me.kyllian.netflixstatistix.exceptions.InputInvalidException;
+import me.kyllian.netflixstatistix.post.PostBuilder;
 import me.kyllian.netflixstatistix.user.User;
 import me.kyllian.netflixstatistix.user.UserBuilder;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.xml.soap.Text;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
-public class ProfileController implements Initializable{
+public class ProfileController extends Controller implements Initializable {
 
     @FXML
     private ImageView logo;
 
     @FXML
     private Label profileMainText;
+
+    private List<Label> texts;
 
     @FXML
     private Label profile1Text;
@@ -64,16 +76,38 @@ public class ProfileController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        texts = new ArrayList<>(Arrays.asList(profile1Text, profile2Text, profile3Text, profile4Text, profile5Text));
+
         logo.setImage(new Image(getClass().getClassLoader().getResource("assets/logo.png").toExternalForm()));
         profilePicture1.setImage(new Image(getClass().getClassLoader().getResource("assets/profilePicture.png").toExternalForm()));
         profilePicture2.setImage(new Image(getClass().getClassLoader().getResource("assets/profilePicture.png").toExternalForm()));
         profilePicture3.setImage(new Image(getClass().getClassLoader().getResource("assets/profilePicture.png").toExternalForm()));
         profilePicture4.setImage(new Image(getClass().getClassLoader().getResource("assets/profilePicture.png").toExternalForm()));
         profilePicture5.setImage(new Image(getClass().getClassLoader().getResource("assets/profilePicture.png").toExternalForm()));
+
+        new PostBuilder().withIdentifier("profiles")
+                .withAttribute("userID", NetflixStatistix.getSessionData().getUserID() + "").post(this);
         //get data
     }
 
-    public void profile(){
+    @Override
+    public void handleResponse(String response) {
+        Platform.runLater(() -> {
+            System.out.println(response);
+            try {
+                JSONArray array = new JSONArray(response);
+                for (int i = 0; i != array.length(); i++) {
+                    JSONObject profile = array.getJSONObject(i);
+                    texts.get(i).setText(profile.getString("profile_name"));
+                }
+            } catch (JSONException exception) {
+                System.out.println("Error reading JSON from server");
+                exception.printStackTrace();
+            }
+        });
+    }
+
+    public void profile() {
 
     }
 }
