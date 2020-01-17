f0ac1b5bab596a70c816abae068065c8698df102
Afrash <32881768+Afrash@users.noreply.github.com>
2020-01-17 22:52:02 +0100

    Made Statistic Controller

---
+++ src/main/java/me/kyllian/netflixstatistix/controllers/statisticController.java
@@ -0,0 +1,8 @@
+package me.kyllian.netflixstatistix.controllers;
+
+public class statisticController extends Controller {
+    @Override
+    public void handleResponse(String response) {
+
+    }
+}
