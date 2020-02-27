95179ba99175ff84ae9f22c50f2e768cff9c4be2
Afrash <32881768+Afrash@users.noreply.github.com>
2020-02-28 00:49:54 +0100

    watchedDataOverview Controller and fxml created

---
+++ src/main/java/me/kyllian/netflixstatistix/controllers/WatchedDataOverviewController.java
@@ -0,0 +1,19 @@
+package me.kyllian.netflixstatistix.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WatchedDataOverviewController extends Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public void handleResponse(String response) {
+
+    }
+}
